8ab75a6bf8a3f7cf664227d04bd64cff7df75eaa
sheilavoon <sheila_voon@yahoo.com>
2023-12-29 21:21:39 -0800

    added functions

--- app.js
+++ app.js
@@ -1,42 +1,87 @@
+
+'use strict'; // will help you to follow strict javascript rules
+
 // FROM THE IN-CLASS VSCODE DEMO:
-console.log("I am in the HTML"); // proof of life to see if our external Javascript is connected to our HTML)
-alert("WELCOME!"); 
+// console.log("I am in the HTML"); // proof of life to see if our external Javascript is connected to our HTML)
+// alert("WELCOME!"); 
 
 // Ask the user a question with prompt
 // Store the user's input (what we get back from prompt) in a variable so that we can refer to it later on.
-const usersName = prompt("What is your name?");
-console.log(usersName);
+// const usersName = prompt("Hi! What is your name?");
+// console.log(usersName);
 
-// string concatenation - chaining data types together
-document.write("Hi " + usersName + "!");
-
-// Ask the user another question with prompt and store
-const response = confirm("Click 'ok' if this your first time here?");
-
-if (response) {
-    // add code if the user pressed the Ok button
-    document.write("<br> Thank you for visiting!");
-} else {
-    // add code if the user pressed the Cancel button
-    document.write("<br> Welcome Back!");
+function getName (){
+  const usersName = prompt("Hi! What is your name?");
+  // declaring a constant variable called usersName, assigning it the value of what the prompt returns (the user's input)
+  return usersName;
 }
 
-// Ask the user another question with prompt and store
-const usersAns = prompt("Are you looking for a furry friend?")
-console.log(usersAns);
+// the getName function is invoked in line 13 of index.html
 
-// CONDITIONAL LOGIC
+function greetUser(){
+  document.write("Hi " + theirName + "!");
+}
 
-// BASIC STRUCTURE
-// if (this is true) {execute this code} else if (this is true){execute this code} else {for literally anything else not previously specified, do this}
+// the greetUser function is invoked in line 14 of index.html
 
-if (usersAns == "Yes"){
-    document.write("<br> Let's get searching!");
-  } else if (usersAns == "yes"){
-    document.write("<br> Let's get searching!");
-  } else if (usersAns == "YES"){
-    document.write("<br> Let's get searching!");
+
+// // Ask the user another question with prompt and store
+// const response = confirm("Click 'ok' if this your first time here?");
+
+// if (response) {
+//     // add code if the user pressed the Ok button
+//     document.write("<br> Thank you for visiting!");
+// } else {
+//     // add code if the user pressed the Cancel button
+//     document.write("<br> Welcome Back!");
+// }
+
+function getResponse(){
+  const usersResponse = confirm("Click 'Ok' if this is your first time here?")
+  if (usersResponse){
+    document.write("<br> Thank you for visiting!");
   } else {
-    document.write("<br> Glad to have you here!");
+    document.write("<br> Welcome Back!");
   }
+}
+
+// the getResponse function is invoked in line 22 of index.html
+
+// // Ask the user another question with prompt and store
+// const usersAns = prompt("Are you looking for a furry friend?")
+// console.log(usersAns);
+
+// // CONDITIONAL LOGIC
+
+// // BASIC STRUCTURE
+// // if (this is true) {execute this code} else if (this is true){execute this code} else {for literally anything else not previously specified, do this}
+
+// if (usersAns == "Yes"){
+//     document.write("<br> Let's get searching!");
+//   } else if (usersAns == "yes"){
+//     document.write("<br> Let's get searching!");
+//   } else if (usersAns == "YES"){
+//     document.write("<br> Let's get searching!");
+//   } else {
+//     document.write("<br> Glad to have you here!");
+//   }
+
+ function letsGoSearching() {
+  document.write("<br> Let's get searching!");
+ }
+  function getAnswer(){
+    const usersAnswer = prompt ("Are you looking for a furry friend?")
+    console.log(usersAnswer);
+    if (usersAnswer == "Yes"){
+      letsGoSearching();
+    } else if (usersAnswer == "yes"){
+      letsGoSearching();
+    } else if (usersAnswer == "YES"){
+      letsGoSearching();
+    } else {
+      document.write("<br> Glad to have you here!");
+    }
+  }
+
+  // the getAnswer function is invoked in line 23 of index.html
 
